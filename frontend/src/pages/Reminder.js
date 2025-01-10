301d508278ae982e29b02240c1d35fd9d668a32f
Ankur Aditya <ankuraditya7@gmail.com>
2025-01-10 22:15:12 +0530

    removing conflict dependancies

--- frontend/src/pages/Reminder.js
+++ frontend/src/pages/Reminder.js
@@ -1,24 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Bar } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+
+
 
 const Reminder = () => {
   const navigate = useNavigate();
